c37dd606c9805af062dfc68d1c6b02ab1fe915ac
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-10-06 21:03:58 -0500

    feed both vectors through one model

--- t5_classifier.py
+++ t5_classifier.py
@@ -13,14 +13,14 @@ class T5Classifier(torch.nn.Module):
         encoder_output1 = self.lang_encoder.encoder(input_ids=ids1, attention_mask=mask1, return_dict=True)
         pooled_sentence1 = encoder_output1.last_hidden_state
         lang_rep_avg1 = torch.mean(pooled_sentence1, 1)
-        print(lang_rep_avg1.size())
+        #print(lang_rep_avg1.size())
         # feed text through t5 then average across encoding dimension and then do two class classification
         encoder_output2 = self.lang_encoder.encoder(input_ids=ids2, attention_mask=mask2, return_dict=True)
         pooled_sentence2 = encoder_output2.last_hidden_state
         lang_rep_avg2 = torch.mean(pooled_sentence2, 1)
-        print(lang_rep_avg2.size())
+        #print(lang_rep_avg2.size())
         both_lang_rep = torch.cat((lang_rep_avg1, lang_rep_avg2), dim=1)
-        print(both_lang_rep.size())
+        #print(both_lang_rep.size())
         output = self.classifier(both_lang_rep)
 
         return output
