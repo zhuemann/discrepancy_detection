90ddf2acb46dde16658abee0760e36b9398b38f1
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-14 13:46:59 -0600

    Different dataset

--- discrepancy_datasetup.py
+++ discrepancy_datasetup.py
@@ -50,7 +50,7 @@ def discrepancy_datasetup(config):
         #print(row['Discrepancy score'])
         #if pd.isna(row['Accession Number']):
         #    continue
-        if pd.isna(row['Discrepancy score']):
+        if pd.isna(row['Discrepancy']):
             discrepancy_that_are_nan += 1
             continue
         #print(row)
