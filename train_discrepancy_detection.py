cc92c246b213463e6b5350f2dacc24153764d136
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-14 23:28:17 -0600

    Different dataset

--- train_discrepancy_detection.py
+++ train_discrepancy_detection.py
@@ -30,7 +30,7 @@ def train_discrepancy_detection(config):
     #df.set_index("id", inplace=True)
 
     #balanced_df = balance_dataset(df)
-    print(balanced_df)
+    #print(balanced_df)
     #print(df)
     #t5_path = os.path.join(dir_base, 'Zach_Analysis/models/t5_large/')
     #tokenizer = T5Tokenizer.from_pretrained(t5_path)
