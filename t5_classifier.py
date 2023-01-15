275ca984d0601caf6085d89fceaf706acdde8978
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-14 18:26:05 -0600

    Different dataset

--- t5_classifier.py
+++ t5_classifier.py
@@ -5,7 +5,9 @@ class T5Classifier(torch.nn.Module):
     def __init__(self, model, n_class = 1):
         super(T5Classifier, self).__init__()
         self.lang_encoder = model
-        self.classifier = torch.nn.Linear(2048, n_class)
+        #self.classifier = torch.nn.Linear(2048, n_class)
+        self.classifier = torch.nn.Linear(1536, n_class)
+
 
     def forward(self, ids1, mask1, ids2, mask2): #, token_type_ids):
 
