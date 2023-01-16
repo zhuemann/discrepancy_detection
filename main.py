696dbd852cf9e48c73b5dd8695f31aa5a27a90a0
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-16 13:08:34 -0600

    Different dataset

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 20, "n_classes": 2, "LR": 1e-5,
+    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 20, "n_classes": 2, "LR": 1e-6,
                   "train_samples": .8, "test_samples": .5, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5
 
     #inference_on_all_data(config)
