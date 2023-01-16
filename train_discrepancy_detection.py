9c71c6a08b579092fa4ab0c0e0af57035897eefe
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-15 23:39:54 -0600

    Different dataset

--- train_discrepancy_detection.py
+++ train_discrepancy_detection.py
@@ -46,10 +46,10 @@ def train_discrepancy_detection(config):
     print("after all is loaded")
 
     for param in language_model1.parameters():
-        param.requires_grad = False
+        param.requires_grad = True
 
     for param in language_model2.parameters():
-        param.requires_grad = False
+        param.requires_grad = True
     #model = T5Classifier(language_model, n_class=1)
     model = RobertaClassifier(language_model1, language_model2, n_class=1)
 
