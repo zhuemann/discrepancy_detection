ed9c8041d1c3a8ef81de27b5d1a21668e330f391
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-18 22:25:04 -0600

    Different dataset

--- inference_on_all_data.py
+++ inference_on_all_data.py
@@ -111,9 +111,9 @@ def inference_on_all_data(config):
     #model = T5Classifier(language_model, n_class=1)
 
     #save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/initial_testing_augmented_data_unbalanced_v6/seed98"
-    save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_dataset_bce_loss_single_model_unbalancedv12/seed98"
+    save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_dataset_bce_loss_single_model_unbalancedv12/seed295"
     save_location = os.path.join(config["dir_base"], save_string)
-    saved_path = os.path.join(save_location, "best_model_seed" + str(98))
+    saved_path = os.path.join(save_location, "best_model_seed" + str(295))
     model.load_state_dict(torch.load(saved_path))
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
