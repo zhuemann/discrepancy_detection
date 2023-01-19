4c7ab710b72a66a5f63c01870167b292162483da
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-19 00:37:55 -0600

    Different dataset

--- inference_on_all_data.py
+++ inference_on_all_data.py
@@ -112,7 +112,7 @@ def inference_on_all_data(config):
     #model = T5Classifier(language_model, n_class=1)
 
     #save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/initial_testing_augmented_data_unbalanced_v6/seed98"
-    save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_dataset_bce_loss_single_model_unbalancedv12/seed" + str(config["seed"])
+    save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_dataset_bce_loss_single_model_unbalanced_no_text_augv13/seed" + str(config["seed"])
     save_location = os.path.join(config["dir_base"], save_string)
     saved_path = os.path.join(save_location, "best_model_seed" + str(config["seed"]))
     model.load_state_dict(torch.load(saved_path))
--- main.py
+++ main.py
@@ -30,17 +30,17 @@ if __name__ == '__main__':
     config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 20, "n_classes": 2, "LR": 5e-6,
                   "train_samples": .8, "test_samples": .5, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6
 
-    config["seed"] = 456
-    inference_on_all_data(config)
+    #config["seed"] = 456
+    #inference_on_all_data(config)
     #print(fail)
-    #seeds = [117, 295, 98, 456, 915, 1367, 712]
+    seeds = [117, 295, 98, 456, 915, 1367, 712]
     #seeds = [117]
-    seeds = [915, 1367, 712]
+    #seeds = [915, 1367, 712]
 
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_dataset_bce_loss_single_model_unbalancedv12/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_dataset_bce_loss_single_model_unbalanced_no_text_augv13/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
@@ -54,4 +54,4 @@ if __name__ == '__main__':
         filepath = os.path.join(config["save_location"], "valid_150ep_seed" + str(seed) + '.xlsx')
         df.to_excel(filepath, index=False)
 
-        inference_on_all_data(config)
+        #inference_on_all_data(config)
