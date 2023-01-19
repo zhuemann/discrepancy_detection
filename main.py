73bca6f17ef3783745ab8bf58ab116f67d47a5ea
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-18 23:32:41 -0600

    Different dataset

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 20, "n_classes": 2, "LR": 5e-6,
                   "train_samples": .8, "test_samples": .5, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6
 
-    #inference_on_all_data(config)
+    inference_on_all_data(config)
     #print(fail)
     #seeds = [117, 295, 98, 456, 915, 1367, 712]
     #seeds = [117]
