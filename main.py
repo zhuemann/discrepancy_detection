2e10dc12fd3949d18bab4334e078af27c64705b2
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-31 12:36:44 -0600

    Different dataset

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/radbert_single_unbalanced_dups_removed_matrixv27/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/radbert_single_v28/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
