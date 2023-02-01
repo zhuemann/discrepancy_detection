6b5de2be10a082614d7bf23e6f2b2a618a2f0d11
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-02-01 12:31:13 -0600

    Different dataset

--- next_setence_prediction.py
+++ next_setence_prediction.py
@@ -26,7 +26,7 @@ def train_discrepancy_detection_nsp(config):
 
     #print(df)
     #dir_base = config["dir_base"]
-    dataframe_location = os.path.join(dir_base, 'Zach_Analysis/discrepancy_data/second_set_binary_discrepancy_df.xlsx')
+    dataframe_location = os.path.join(dir_base, 'Zach_Analysis/discrepancy_data/second_set_matches_removed_dups_removed_hand_cleaned_df.xlsx')
     #dataframe_location = os.path.join(dir_base, 'Zach_Analysis/discrepancy_data/second_set_binary_discrepancy_balanced_df.xlsx')
 
     df = pd.read_excel(dataframe_location, engine='openpyxl')
