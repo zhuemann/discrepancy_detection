ee8523ea32a92fe09a62c0942a9ad1c0b129b9f2
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-02-01 15:12:31 -0600

    Different dataset

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 20, "n_classes": 2, "LR": 5e-6,
+    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 15, "n_classes": 2, "LR": 5e-6,
                   "train_samples": .75, "valid_samples": .4, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6 5e-6 is best
 
     #config["seed"] = 456
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/radbert_double_v2_v34/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/radbert_double_v2_v35/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
