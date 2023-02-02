fbeb7f8869f83b32ff84a257f654de7af2bc3c50
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-02-02 12:38:47 -0600

    Different dataset

--- discrepancy_datasetup.py
+++ discrepancy_datasetup.py
@@ -12,8 +12,8 @@ def discrepancy_datasetup(config):
     dataframe_location = os.path.join(dir_base,'Zach_Analysis/discrepancy_data/second_labeled_batch.xlsx')
     #dataframe_location = os.path.join(dir_base,'Zach_Analysis/discrepancy_data/first_labeled_batch.xlsx')
 
-    df = pd.concat(pd.read_excel(dataframe_location, sheet_name=None, engine='openpyxl'), ignore_index=True)
-    #df = pd.read_excel(dataframe_location, engine='openpyxl')
+    #df = pd.concat(pd.read_excel(dataframe_location, sheet_name=None, engine='openpyxl'), ignore_index=True)
+    df = pd.read_excel(dataframe_location, engine='openpyxl')
     #print(df)
     df = df.dropna(axis=0, how='all')
     #print(df)
