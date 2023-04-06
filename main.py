0ada1033e3edc059fcaa77deead43289e7668911
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-06 17:07:21 -0500

    Running over all datapooled seeds

--- main.py
+++ main.py
@@ -41,7 +41,9 @@ if __name__ == '__main__':
     #count_duplicates(config)
     #pick_test_set(config)
     #print(fail)
-    seeds = [117, 295, 98, 456, 915, 1367, 712]
+    #seeds = [117, 295, 98, 456, 915, 1367, 712]
+    seeds = [295, 98, 456, 915, 1367, 712]
+
     #seeds = [117, 295]
     #seeds = [915, 1367, 712]
     #seeds = [117]
