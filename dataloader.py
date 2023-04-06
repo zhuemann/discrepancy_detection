0e7d476c5a12af9a7f73f71c4172c524de8a362f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-06 17:02:40 -0500

    Getting dataframe to copy into train data

--- dataloader.py
+++ dataloader.py
@@ -142,11 +142,11 @@ def setup_dataloader(df, config, tokenizer, wordDict=None):
     #print(fail)
     load_df_from_preset_location = True
     if load_df_from_preset_location:
-        train_loc = os.path.join(dir_base, 'Zach_Analysis/discrepancy_data/used_to_train_second_model/datafolder/seed' +str(config["seed"]) + '/train_df_seed' +str(config["seed"]) + '.xlsx')
+        train_loc = os.path.join(dir_base, 'Zach_Analysis/discrepancy_data/second_labeling_batch/datafolder/seed' +str(config["seed"]) + '/train_df_seed' +str(config["seed"]) + '.xlsx')
         train_df = pd.read_excel(train_loc, engine='openpyxl')
-        valid_loc = os.path.join(dir_base,'Zach_Analysis/discrepancy_data/used_to_train_second_model/datafolder/seed' +str(config["seed"]) + '/valid_df_seed' +str(config["seed"]) + '.xlsx')
+        valid_loc = os.path.join(dir_base,'Zach_Analysis/discrepancy_data/second_labeling_batch/datafolder/seed' +str(config["seed"]) + '/valid_df_seed' +str(config["seed"]) + '.xlsx')
         valid_df = pd.read_excel(valid_loc, engine='openpyxl')
-        test_loc = os.path.join(dir_base,'Zach_Analysis/discrepancy_data/used_to_train_second_model/datafolder/seed' +str(config["seed"]) + '/test_df_seed' +str(config["seed"]) + '.xlsx')
+        test_loc = os.path.join(dir_base,'Zach_Analysis/discrepancy_data/second_labeling_batch/datafolder/seed' +str(config["seed"]) + '/test_df_seed' +str(config["seed"]) + '.xlsx')
         test_df = pd.read_excel(test_loc, engine='openpyxl')
 
     training_set = TextDataset(train_df, tokenizer, dir_base=dir_base, wordDict= wordDict)
