2af51aab4572e1cffc2113821dbe136ab26ee9dc
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-09 02:10:33 -0500

    Training with the base positive cases and then sampling the extras

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 35, "n_classes": 2, "LR": 5e-6,
+    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 25, "n_classes": 2, "LR": 5e-6,
                   "train_samples": .75, "valid_samples": .4, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6 5e-6 is best
 
     #config["seed"] = 117
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_labeling_batch/radbert_training_sampler_v54/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_labeling_batch/radbert_training_sampler_v55/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
