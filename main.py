07b964fe1218a4b1a557ea3b4260e5fd8965329c
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-09 16:40:25 -0500

    Training second step, fine tuning on original data starting from trained model on the pooled data

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 20, "n_classes": 2, "LR": 5e-6,
+    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 15, "n_classes": 2, "LR": 1e-6,
                   "train_samples": .75, "valid_samples": .4, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6 5e-6 is best
-
+    # best results for far are with lr 5e-6 and 20 epochs
     #config["seed"] = 117
     #inference_on_all_data(config)
     #print(fail)
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_labeling_batch/radbert_fine_tuning_with_less_data_v58/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/second_labeling_batch/radbert_fine_tuning_with_less_data_v59/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
