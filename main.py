4993f1da5e4bfed080b9beb4fa7217607e01e841
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-10 14:54:37 -0500

    pooling all data in training run v70

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ if __name__ == '__main__':
     #count_duplicates(config)
     #pick_test_set(config)
     #print(fail)
-    #seeds = [98, 117, 295, 456, 712, 915, 1367]
+    seeds = [98, 117, 295, 456, 712, 915, 1367]
     #seeds = [295, 98, 456, 915, 1367, 712]
-    seeds = [712]
+    #seeds = [712]
 
     #seeds = [117, 295]
     #seeds = [915, 1367, 712]
