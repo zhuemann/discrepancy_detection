6c65d50199d0278ef02b26efa5afd6d1a0c993fa
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-28 16:20:07 -0500

    Training with final model pooled data v80

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 12, "n_classes": 2, "LR": 1e-6,
+    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 15, "n_classes": 2, "LR": 5e-6,
                   "train_samples": .75, "valid_samples": .4, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6 5e-6 is best
     # best results for far are with lr 5e-6 and 20 epochs
     #config["seed"] = 456
