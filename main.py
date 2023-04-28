ca061e97707361fc09d5e7112ba4a488b2334071
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-28 10:39:49 -0500

    Training with third dataset v74 fine tuning v73

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 15, "n_classes": 2, "LR": 5e-6,
+    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 10, "n_classes": 2, "LR": 1e-6,
                   "train_samples": .75, "valid_samples": .4, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6 5e-6 is best
     # best results for far are with lr 5e-6 and 20 epochs
     #config["seed"] = 456
@@ -50,14 +50,14 @@ if __name__ == '__main__':
     #seeds = [456, 712, 915, 1367]
     #seeds = [915, 1367, 712]
     #seeds = [117]
-    seeds = [98, 117, 295]
+    #seeds = [98, 117, 295]
     #seeds = [712]
     accur_list = []
 
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_pool_data_v73/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_pool_data_finetuning_v74/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
