f5eb486b7be48407ed525cecedeaef7777c5e345
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-28 12:33:04 -0500

    Training with third dataset v76 trained on only new labels

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_pool_data_finetuning_v75/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_pool_new_labels_v76/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
