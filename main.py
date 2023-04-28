b8d277edc8a635a949ef18f09d26ef8a9933e3a3
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-28 13:52:59 -0500

    Training with third dataset fine tuning v76 on original dataset v78

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 10, "n_classes": 2, "LR": 5e-6,
+    config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 15, "n_classes": 2, "LR": 5e-6,
                   "train_samples": .75, "valid_samples": .4, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6 5e-6 is best
     # best results for far are with lr 5e-6 and 20 epochs
     #config["seed"] = 456
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_fine_turning_original_v77/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_fine_turning_original_v78/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
