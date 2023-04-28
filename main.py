7f1e43335b0ed45e20fd4278e1f79ca3e0ae46f8
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-28 17:02:05 -0500

    Getting final test frame

--- main.py
+++ main.py
@@ -35,8 +35,12 @@ if __name__ == '__main__':
 
     config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 15, "n_classes": 2, "LR": 5e-6,
                   "train_samples": .75, "valid_samples": .4, "data_path": "D:/candid_ptx/"} #was .8 .5 lr was 1e-5 5e-6 5e-6 is best
+
     # best results for far are with lr 5e-6 and 20 epochs
     config["seed"] = 117
+    save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_final_model_first_train_v81/" + folder_name
+    save_location = os.path.join(directory_base, save_string)
+    config["save_location"] = save_location
     inference_on_all_data(config)
     print(fail)
     #discrepancy_datasetup(config)
