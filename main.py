b67a0ba8a4d3235920748b81f429cb7ac7c7c44d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-28 17:32:10 -0500

    Doing final tine tuning

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     for seed in seeds:
 
         folder_name = "seed" + str(seed) + "/"
-        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_final_model_first_train_v81/" + folder_name
+        save_string = "/UserData/Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/radbert_final_model_second_train_finetune_v81/" + folder_name
         save_location = os.path.join(directory_base, save_string)
 
         config["seed"] = seed
