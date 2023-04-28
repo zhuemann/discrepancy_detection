da6a456e4df2211bb0c08703d296f720cf21d7dc
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-04-28 17:31:00 -0500

    Doing final tine tuning

--- dataloader.py
+++ dataloader.py
@@ -135,7 +135,7 @@ def setup_dataloader(df, config, tokenizer, wordDict=None):
 
         train_df = pd.read_excel(train_loc, engine='openpyxl')
         #valid_loc = os.path.join(dir_base,'Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/data_folder_finetuning/seed' +str(config["seed"]) + '/valid_df_seed' +str(config["seed"]) + '.xlsx')
-        valid_loc = os.path.join(dir_base,'Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/data_folder_updated_final_train/seed117/seed' +str(config["seed"]) + '/valid_df_seed' +str(config["seed"]) + '.xlsx')
+        valid_loc = os.path.join(dir_base,'Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/data_folder_updated_final_train/seed' +str(config["seed"]) + '/valid_df_seed' +str(config["seed"]) + '.xlsx')
         valid_df = pd.read_excel(valid_loc, engine='openpyxl')
         test_loc = os.path.join(dir_base,'Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/data_folder_updated/seed' +str(config["seed"]) + '/test_df_seed' +str(config["seed"]) + '.xlsx')
         test_df = pd.read_excel(test_loc, engine='openpyxl')
