f91e3ff30f42a7b0db7995e5fcbf8e07006bcbd9
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-05-01 14:28:04 -0500

    finetuning on v86 attempt number two fixed dataloader bug v88

--- dataloader.py
+++ dataloader.py
@@ -128,7 +128,7 @@ def setup_dataloader(df, config, tokenizer, wordDict=None):
     test_df.set_index("id", inplace=True)
 
     #print(fail)
-    load_df_from_preset_location = True
+    load_df_from_preset_location = False
     if load_df_from_preset_location:
         #train_loc = os.path.join(dir_base, 'Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/data_folder_updated_final_train/seed' +str(config["seed"]) + '/train_df_seed' +str(config["seed"]) + '.xlsx')
         #train_loc = os.path.join(dir_base, 'Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/data_folder_updated/second_and_third_labeled_df'+ '.xlsx')
@@ -145,7 +145,7 @@ def setup_dataloader(df, config, tokenizer, wordDict=None):
         test_loc = os.path.join(dir_base,'Zach_Analysis/result_logs/discrepancy_detection/third_labeling_batch/data_folder_updated_v1/seed' +str(config["seed"]) + '/test_df_seed' +str(config["seed"]) + '.xlsx')
         test_df = pd.read_excel(test_loc, engine='openpyxl')
 
-    fine_tuning = False
+    fine_tuning = True
     if fine_tuning:
 
         train_loc = os.path.join(dir_base,
